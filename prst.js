5e906b4cf4053b959349ed75aa1c781781b1546b
preethi d <preethisaranya858@gmail.com>
2026-04-29 14:01:22 +0530

    updated sucessfully

--- prst.js
+++ prst.js
@@ -1,3 +1,17 @@
-function changeText() {
-    document.getElementById("text").innerHTML = "Text Changed!";
+function addProject() {
+    let input = document.getElementById("projectInput").value;
+
+    if (input === "") {
+        alert("Enter a project name");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.textContent = input;
+
+    document.getElementById("projectList").appendChild(li);
+
+    document.getElementById("status").innerHTML = "Projects Updated";
+
+    document.getElementById("projectInput").value = "";
 }
